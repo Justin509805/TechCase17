ac201d662c3e9f043af2e90841bb45041b43fe7a
Justin509805 <509805@student.fontys.nl>
2024-03-20 11:07:25 +0100

    added dot, dash button, it translates only letters

--- lib/main.dart
+++ lib/main.dart
@@ -6,35 +6,22 @@ void main() {
 
 class MorseCodeTranslator {
   static Map<String, String> morseCodeMap = {
-    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
-    'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
-    'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---',
-    'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
-    'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--',
-    'Z': '--..', '0': '-----', '1': '.----', '2': '..---', '3': '...--',
-    '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..',
-    '9': '----.', ' ': '/'
+    '.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D', '.': 'E',
+    '..-.': 'F', '--.': 'G', '....': 'H', '..': 'I', '.---': 'J',
+    '-.-': 'K', '.-..': 'L', '--': 'M', '-.': 'N', '---': 'O',
+    '.--.': 'P', '--.-': 'Q', '.-.': 'R', '...': 'S', '-': 'T',
+    '..-': 'U', '...-': 'V', '.--': 'W', '-..-': 'X', '-.--': 'Y',
+    '--..': 'Z', '-----': '0', '.----': '1', '..---': '2', '...--': '3',
+    '....-': '4', '.....': '5', '-....': '6', '--...': '7', '---..': '8',
+    '----.': '9', '/': ' '
   };
 
-  static Map<String, String> reverseMorseCodeMap =
-      morseCodeMap.map((key, value) => MapEntry(value, key));
-
-  static String textToMorseCode(String text) {
-    List<String> result = [];
-    for (var char in text.toUpperCase().split('')) {
-      if (morseCodeMap.containsKey(char)) {
-        result.add(morseCodeMap[char]!);
-      }
-    }
-    return result.join(' ');
-  }
-
   static String morseCodeToText(String morseCode) {
-    List<String> morseList = morseCode.split(' ');
+    List<String> morseList = morseCode.trim().split(' ');
     List<String> result = [];
     for (var code in morseList) {
-      if (reverseMorseCodeMap.containsKey(code)) {
-        result.add(reverseMorseCodeMap[code]!);
+      if (morseCodeMap.containsKey(code)) {
+        result.add(morseCodeMap[code]!);
       }
     }
     return result.join('');
@@ -58,7 +45,23 @@ class MorseCodeApp extends StatefulWidget {
 class _MorseCodeAppState extends State<MorseCodeApp> {
   String inputText = '';
   String morseCode = '';
-  bool signalingText = true;
+
+  void translateInputToMorseCode(String signal) {
+    setState(() {
+      if (signal == '.') {
+        morseCode += '.';
+      } else if (signal == '-') {
+        morseCode += '-';
+      }
+    });
+  }
+
+  void decodeMorseCode() {
+    setState(() {
+      inputText = MorseCodeTranslator.morseCodeToText(morseCode);
+      morseCode = '';
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -71,45 +74,50 @@ class _MorseCodeAppState extends State<MorseCodeApp> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              signalingText ? 'Input Text:' : 'Morse Code:',
-              style: TextStyle(fontSize: 18),
-            ),
             TextField(
               onChanged: (value) {
                 setState(() {
                   inputText = value;
-                  if (signalingText) {
-                    morseCode = MorseCodeTranslator.textToMorseCode(inputText);
-                  }
                 });
               },
               decoration: InputDecoration(
-                hintText: signalingText ? 'Enter text' : 'Enter Morse code',
+                hintText: 'Enter Morse code',
               ),
             ),
             SizedBox(height: 20),
-            Text(
-              signalingText ? 'Morse Code:' : 'Decoded Text:',
-              style: TextStyle(fontSize: 18),
-            ),
-            Text(
-              signalingText ? morseCode : MorseCodeTranslator.morseCodeToText(morseCode),
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    translateInputToMorseCode('.');
+                  },
+                  child: Text('Dot'),
+                ),
+                SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    translateInputToMorseCode('-');
+                  },
+                  child: Text('Dash'),
+                ),
+              ],
             ),
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
-                setState(() {
-                  signalingText = !signalingText;
-                  if (signalingText) {
-                    morseCode = MorseCodeTranslator.textToMorseCode(inputText);
-                  } else {
-                    inputText = MorseCodeTranslator.morseCodeToText(morseCode);
-                  }
-                });
+                decodeMorseCode();
               },
-              child: Text(signalingText ? 'Signal Morse Code' : 'Translate to Text'),
+              child: Text('Decode Morse Code'),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Decoded Text:',
+              style: TextStyle(fontSize: 18),
+            ),
+            Text(
+              inputText,
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
             ),
           ],
         ),
@@ -117,4 +125,3 @@ class _MorseCodeAppState extends State<MorseCodeApp> {
     );
   }
 }
-
